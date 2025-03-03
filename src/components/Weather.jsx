ac2c3fdf8baace3fcc6eaa7429c172a9d572d0c7
Vincent Tao <vincent.jh.tao@gmail.com>
2025-03-04 00:45:33 +1300

    added css styles to basic weather ui, added weather image assets

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 import "./Weather.css";
+import search_icon from "../assets/search.png";
+import clear_icon from "../assets/clear.png";
 
 const Weather = () => {
   return (
-    <div>
-      <h1>Weather App</h1>
+    <div className="weather">
+      <div className="search-bar">
+        <input type="text" placeholder="Search" />
+        <img src={search_icon} alt="" />
+      </div>
+      <img src={clear_icon} alt="" className="weather-icon" />
+      <p className="temperature">16°C</p>
+      <p className="location">Auckland</p>
     </div>
   );
 };
