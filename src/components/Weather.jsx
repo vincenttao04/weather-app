4c609193cea5b9e0495d3639f85b728700eef996
Vincent Tao <vincent.jh.tao@gmail.com>
2025-03-07 01:22:38 +1300

    clear search bar input after each successful search

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,39 +1,14 @@
 import React, { useEffect, useRef, useState } from "react";
 import "./Weather.css";
 import search_icon from "../assets/search.png";
-import clear_icon from "../assets/clear.png";
-import cloud_icon from "../assets/cloud.png";
-import drizzle_icon from "../assets/drizzle.png";
 import humidity_icon from "../assets/humidity.png";
-import rain_icon from "../assets/rain.png";
-import snow_icon from "../assets/snow.png";
 import wind_icon from "../assets/wind.png";
+import { weatherIcons } from "../config/icons";
 
 const Weather = () => {
   const inputRef = useRef();
   const [weatherData, setWeatherData] = useState(null);
   const [inputValue, setInputValue] = useState("");
-
-  const allIcons = {
-    "01d": clear_icon,
-    "01n": clear_icon,
-    "02d": cloud_icon,
-    "02n": cloud_icon,
-    "03d": cloud_icon,
-    "03n": cloud_icon,
-    "04d": drizzle_icon,
-    "04n": drizzle_icon,
-    "09d": rain_icon,
-    "09n": rain_icon,
-    "10d": rain_icon,
-    "10n": rain_icon,
-    "11d": rain_icon,
-    "11n": rain_icon,
-    "50d": rain_icon,
-    "50n": rain_icon,
-    "13d": snow_icon,
-    "13n": snow_icon,
-  };
 
   const search = async (city) => {
     // Checks OpenWeather API key validity
@@ -54,7 +29,7 @@ const Weather = () => {
 
       console.log(data);
 
-      const icon = allIcons[data.weather[0].icon] || clear_icon;
+      const icon = weatherIcons[data.weather[0].icon] || weatherIcons["01d"];
 
       setWeatherData({
         humidity: data.main.humidity,
@@ -63,6 +38,9 @@ const Weather = () => {
         location: data.name,
         icon: icon,
       });
+
+      // Reset input field to empty string after a successful search
+      setInputValue("");
     } catch {
       setWeatherData(false);
       console.error("Error in fetching weather data");
