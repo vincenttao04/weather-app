8f304bfd54cb3c9208d785e8d73069020c52eb6a
Vincent Tao <vincent.jh.tao@gmail.com>
2025-03-13 01:53:04 +1300

    refactored weather.jsx code for better readability

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -7,52 +7,27 @@ import SearchBar from "./SearchBar.jsx";
 import moment from "moment";
 
 const Weather = () => {
-  const [weatherDataOne, setWeatherDataOne] = useState(null);
-  const [weatherDataTwo, setWeatherDataTwo] = useState(null);
-  const [weatherDataThree, setWeatherDataThree] = useState(null);
+  const [weatherData, setWeatherData] = useState([]);
   const [inputValue, setInputValue] = useState("");
 
   const search = async (city) => {
-    // calls openweather api
+    // Calls openweather api
     const data = await fetchWeather(city);
 
-    if (data !== null) {
-      console.log(data);
-      const iconOne =
-        weatherIcons[data?.list[0]?.weather[0]?.icon] || weatherIcons["01d"];
+    if (!data) return;
 
-      const iconTwo =
-        weatherIcons[data?.list[8]?.weather[0]?.icon] || weatherIcons["01d"];
-      const iconThree =
-        weatherIcons[data?.list[16]?.weather[0]?.icon] || weatherIcons["01d"];
+    const forecastIndexes = [0, 8, 16]; // Current day, tomorrow, 2 days from now
+    const formattedData = forecastIndexes.map((index) => ({
+      humidity: data.list[index]?.main.humidity,
+      windSpeed: data.list[index]?.wind.speed,
+      temperature: Math.round(data.list[index]?.main.temp),
+      location: data.city.name,
+      icon:
+        weatherIcons[data.list[index]?.weather[0]?.icon] || weatherIcons["01d"],
+      date: moment.unix(data.list[index]?.dt).format("dddd D MMMM"),
+    }));
 
-      setWeatherDataOne({
-        humidity: data.list[0].main.humidity,
-        windSpeed: data.list[0].wind.speed,
-        temperature: Math.round(data.list[0].main.temp),
-        location: data.city.name,
-        icon: iconOne,
-        date: moment.unix(data.list[0].dt).format("dddd D MMMM"),
-      });
-
-      setWeatherDataTwo({
-        humidity: data.list[8].main.humidity,
-        windSpeed: data.list[8].wind.speed,
-        temperature: Math.round(data.list[8].main.temp),
-        location: data.city.name,
-        icon: iconTwo,
-        date: moment.unix(data.list[8].dt).format("dddd D MMMM"),
-      });
-
-      setWeatherDataThree({
-        humidity: data.list[16].main.humidity,
-        windSpeed: data.list[16].wind.speed,
-        temperature: Math.round(data.list[16].main.temp),
-        location: data.city.name,
-        icon: iconThree,
-        date: moment.unix(data.list[16].dt).format("dddd D MMMM"),
-      });
-    }
+    setWeatherData(formattedData);
 
     // Reset input field to empty string after a successful search
     setInputValue("");
@@ -66,31 +41,17 @@ const Weather = () => {
   return (
     <div className="root-container">
       <div className="weather-wrapper">
-        <div className="weather">
-          {weatherDataOne ? (
-            <WeatherData weatherData={weatherDataOne} />
-          ) : (
-            <></>
-          )}
-        </div>
-        <div className="weather">
-          {weatherDataTwo ? (
-            <WeatherData weatherData={weatherDataTwo} />
-          ) : (
-            <></>
-          )}
-        </div>
-        <div className="weather">
-          {weatherDataThree ? (
-            <WeatherData weatherData={weatherDataThree} />
-          ) : (
-            <></>
-          )}
-        </div>
+        {weatherData.map((data, index) => (
+          <div className="weather" key={index}>
+            <WeatherData weatherData={data} />
+          </div>
+        ))}
       </div>
-      <div className="location">
-        {weatherDataOne ? <p>{weatherDataOne.location}</p> : <></>}
-      </div>
+      {weatherData.length > 0 && (
+        <div className="location">
+          <p>{weatherData[0].location}</p>
+        </div>
+      )}
       <SearchBar
         inputValue={inputValue}
         setInputValue={setInputValue}
