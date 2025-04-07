33994f3ba5d7c3433f9132cb8e28ab9cb603f76d
Vincent Tao <vincent.jh.tao@gmail.com>
2025-04-08 03:23:06 +1200

    refactored css code

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { styled } from "@mui/material/styles";
 import "../styles/theme-toggle.css";
 import Switch from "@mui/material/Switch";
 import DarkModeRoundedIcon from "@mui/icons-material/DarkModeRounded";
@@ -9,24 +8,15 @@ import { useTheme } from "@mui/material";
 const ThemeToggle = ({ checked, onChange }) => {
   const theme = useTheme();
 
-  const CustomSwitch = styled(Switch)({
-    "& .MuiSwitch-switchBase": {
-      padding: 4, // Default: 9
-      transform: "translateX(5px) translateY(5px)", // Adjust translation for the unchecked state
-      "&.Mui-checked": {
-        transform: "translateX(25px) translateY(5px)", // Adjust translation for checked state
-      },
-    },
-  });
-
   return (
-    <CustomSwitch
+    <Switch
       className="theme-toggle-switch"
       checked={checked}
       color="default"
       onChange={onChange}
       icon={
         <LightModeRoundedIcon
+          className="light-mode"
           style={{
             width: "20px",
             height: "20px",
@@ -38,6 +28,7 @@ const ThemeToggle = ({ checked, onChange }) => {
       }
       checkedIcon={
         <DarkModeRoundedIcon
+          className="dark-mode"
           style={{
             width: "20px",
             height: "20px",
