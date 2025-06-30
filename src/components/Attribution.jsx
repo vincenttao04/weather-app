3e98d18034409e11644bae470a06b436d6a3ae58
vincenttao04 <vincent.jh.tao@gmail.com>
2025-06-30 21:34:52 +1200

    style: amended attribution colour

--- src/components/Attribution.jsx
+++ src/components/Attribution.jsx
@@ -1,8 +1,9 @@
 import { SiJavascript, SiReact } from "react-icons/si";
 import { Stack, useTheme } from "@mui/material";
-import AddRoundedIcon from "@mui/icons-material/AddRounded";
 
 const Attribution = () => {
+  const theme = useTheme();
+
   return (
     <Stack
       direction="row"
@@ -11,11 +12,13 @@ const Attribution = () => {
         position: "absolute",
         bottom: "36px",
         left: "42px",
+        color: theme.palette.text.secondary,
       }}
     >
-      Vincent Tao |
+      <p>Vincent Tao |</p>
       <SiJavascript size={20} style={{ marginLeft: "6px" }} />
-      <AddRoundedIcon fontSize={"small"} sx={{ mr: 0.5, ml: 0.5 }} />
+      {/* <AddRoundedIcon fontSize={"small"} sx={{ mr: 0.5, ml: 0.5 }} /> */}
+      <p style={{ padding: "0px 4px", fontSize: "22px" }}>+</p>
       <SiReact size={20} />
     </Stack>
   );
