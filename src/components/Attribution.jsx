04f6afb58178ce1cec58984c1b893e6c1dd83fd6
vincenttao04 <vincent.jh.tao@gmail.com>
2025-06-30 21:24:19 +1200

    feat: attribution component

--- src/components/Attribution.jsx
+++ src/components/Attribution.jsx
@@ -1,11 +1,23 @@
 import { SiJavascript, SiReact } from "react-icons/si";
+import { Stack, useTheme } from "@mui/material";
+import AddRoundedIcon from "@mui/icons-material/AddRounded";
 
 const Attribution = () => {
   return (
-    <>
-      <SiJavascript />
-      <SiReact />
-    </>
+    <Stack
+      direction="row"
+      sx={{
+        alignItems: "center",
+        position: "absolute",
+        bottom: "36px",
+        left: "42px",
+      }}
+    >
+      Vincent Tao |
+      <SiJavascript size={20} style={{ marginLeft: "6px" }} />
+      <AddRoundedIcon fontSize={"small"} sx={{ mr: 0.5, ml: 0.5 }} />
+      <SiReact size={20} />
+    </Stack>
   );
 };
 
